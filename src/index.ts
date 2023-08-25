dc955f0e21c56f5fbca15d8a3fb9fafab1e9f095
🚀thanhnguyen <thanhnguyen95abc@gmail.com>
2023-08-25 14:34:28 +0700

    clone  node cluster

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import express from "express";
+import bodyParser from "body-parser";
+import bodyParserXML from "body-parser-xml";
+import router from "./routes";
+import { getAppConfig } from "./config/app_configs";
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import cluster from "cluster";
+import { cpus } from "os";
+import logger from "./utils/logger";
+
+dayjs.extend(customParseFormat);
+bodyParserXML(bodyParser);
+
+const os = cpus().length;
+
+const createApplication = () => {
+  const app = express();
+
+  const CONFIG = getAppConfig();
+
+  app.use(bodyParser.json({ limit: "50mb" }));
+  app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+
+  app.use("/", router);
+
+  app.listen(CONFIG["PORT"], async () => {
+    console.log(`Server started on host : ${CONFIG["HOST"]}:${CONFIG["PORT"]}`);
+  });
+
+  process.on("uncaughtException", (error) => {
+    logger.error(error.stack);
+  });
+};
+
+if (cluster.isPrimary) {
+  for (let i = 0; i < os; i++) {
+    cluster.fork();
+    console.log(`The Worker number: ${i + 1} is alive`);
+  }
+  cluster.on("exit", (worker) => {
+    console.log(`The Worker number: ${worker.id} has died`);
+  });
+} else {
+  createApplication();
+}
+
+export default {};
