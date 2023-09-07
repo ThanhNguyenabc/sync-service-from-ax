bd04a2d214b46dbcb842c8185101bc7caa41670f
🚀thanhnguyen <thanhnguyen95abc@gmail.com>
2023-09-07 14:50:15 +0700

    update model

--- src/models/ax_teacher_profile.model.ts
+++ src/models/ax_teacher_profile.model.ts
@@ -2,13 +2,9 @@ export interface AXTeacherProfile {
   FirstName?: string;
   MiddleName?: string;
   LastName?: string;
-  DOB?: string;
-  Phone?: string;
   Email?: string;
   UserName?: string;
   Role?: "Teacher" | "TA";
-  OfficeLocation?: string;
-  TeacherCode?: string;
-  Address?: string;
-  Status?: string;
+  Active: "Yes" | "No";
+  StaffCode?: string;
 }
