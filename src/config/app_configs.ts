e0d2b5b5b6996ea9378b15a3f4a0fe6261ddd646
🚀thanhnguyen <thanhnguyen95abc@gmail.com>
2023-09-12 09:54:42 +0700

    remove ini module

--- src/config/app_configs.ts
+++ src/config/app_configs.ts
@@ -1,18 +1,9 @@
+import { fetchProgramConfig } from "../apis";
 import AppConfig from "./configs.json";
-import ini from "ini";
-import { readFile } from "fs/promises";
+
 export const getAppConfig = () => {
   const ENV = process.env.NODE_ENV || "development";
   return AppConfig[ENV as keyof typeof AppConfig];
 };
 
-export const getProgramConfig = async () => {
-  try {
-    const programFile = await readFile(`${__dirname}/programs.cfg`, {
-      encoding: "utf-8",
-    });
-    const config = ini.parse(programFile);
-    return config;
-  } catch (error) {}
-  return null;
-};
+export const getProgramConfig = async () => await fetchProgramConfig();
