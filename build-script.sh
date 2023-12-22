197f538bbc9ef4d8f60fd35cdb2a5e1f427ec58a
🚀thanhnguyen <thanhnguyen95abc@gmail.com>
2023-12-22 17:52:51 +0700

    update permission

--- build-script.sh
+++ build-script.sh
@@ -7,9 +7,13 @@ mkdir bitnami/kafka/data
 mkdir bitnami/kafka/config
 sudo chmod u+rwx,g+rwx,o+rx bitnami
 sudo chmod u+rwx,g+rwx,o+rx bitnami/kafka
-
 mkdir logs
 sudo chmod u+rwx,g+rwx,o+rx logs
+
+
+RUN chown -R 1001:1001 bitnami
+RUN chown -R 1001:1001 logs
+
 
 # run docker to build app
 docker compose -f docker-compose.prod.yml up -d
