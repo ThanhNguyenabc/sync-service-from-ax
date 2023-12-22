49633e8266b69350a3a7e4e930babbc8af1e0db3
🚀thanhnguyen <thanhnguyen95abc@gmail.com>
2023-12-22 15:39:22 +0700

    update build script

--- build-script.sh
+++ build-script.sh
@@ -2,8 +2,12 @@
 
 # create folders and allow their permission
 mkdir bitnami
-mkdir logs
+mkdir bitnami/kafka/data
+mkdir bitnami/kafka/config
 sudo chmod u+rwx,g+rwx,o+rx bitnami
+sudo chmod u+rwx,g+rwx,o+rx bitnami/kafka
+
+mkdir logs
 sudo chmod u+rwx,g+rwx,o+rx logs
 
 # run docker to build app
