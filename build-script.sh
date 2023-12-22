e1b7b7e9c0d3d0fc75dcd73a6b88c9b8c8964777
🚀thanhnguyen <thanhnguyen95abc@gmail.com>
2023-12-22 15:29:18 +0700

    update build script

--- build-script.sh
+++ build-script.sh
@@ -3,8 +3,8 @@
 # create folders and allow their permission
 mkdir bitnami
 mkdir logs
-sudo chmod a+w bitnami
-sudo chmod a+w logs
+sudo chmod u+rwx,g+rwx,o+rx bitnami
+sudo chmod u+rwx,g+rwx,o+rx logs
 
 # run docker to build app
 docker compose -f docker-compose.prod.yml up -d
