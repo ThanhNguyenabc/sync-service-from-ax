238d155b58db2f3b696c84bd863556d64d1c37b7
thanhnguyen <thanhnguyen95abc@gmail.com>
2024-06-03 11:25:47 +0700

    change permission for docker

--- build-script.sh
+++ build-script.sh
@@ -4,10 +4,6 @@
 mkdir bitnami
 mkdir logs
 
-# chmod -R g+rwX 
-sudo chmod -R g+rwX bitnami
-sudo chmod -R g+rwX logs
-
 
 # run docker to build app
-docker compose -f docker-compose.prod.yml up -d --build app
+docker compose -f docker-compose.prod.yml up -d
